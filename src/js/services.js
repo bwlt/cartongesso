1948d76bbfcf768629ffd147a439a8d77114d088
Walter Barbagallo <turbometalskater@gmail.com>
2014-12-03 18:30:57 +0100

    implemented business logic

--- src/js/services.js
+++ src/js/services.js
@@ -11,7 +11,9 @@ var appServices = angular.module('cartongessoServices', []);
 appServices.factory('currencyAdapter', function(){
 
     var getCurrencyValue = function(number) {
-        return Math.round(Number(number) * 100) / 100;
+        var numberObj = Number(number);
+
+        return Number.isNaN(numberObj) ? 0 : Math.round(numberObj * 100) / 100;
     };
 
     return {
@@ -31,27 +33,106 @@ appServices.factory('cartongessoCalculator', ['currencyAdapter', function(curren
      * @param price float the montante price [euro@m]
      * @param interasse float distance between montanti [m]
      */
-    var calcMontanti = function(price, interasse) {
-        return currencyAdapter.getCurrencyValue(price/interasse);
+    var calcMontanti = function(interasse, price) {
+        return interasse == '0' ? 0 :
+            currencyAdapter.getCurrencyValue(price/interasse);
     };
 
     /**
-     * @param b number the montante price [euro@m]
-     * @param interasse number distance between montanti [m]
+     * @param b integer width [m]
+     * @param h integer height [m]
+     * @param price float the guide price [euro@m]
      */
     var calcGuide = function(b, h, price) {
+        if (b == '0' || h == '0') {
+            return 0;
+        }
         return currencyAdapter.getCurrencyValue(
-            2 * (b + h) * price / (b * h)
-            );
+            2 * (b + h) * price / (b * h));
     };
 
-    var getResult = function(){
-        return 'I am getResult';
+    /**
+     * @param num integer the number of lastre [dimesionless]
+     * @param price float the lastre price [euro@m]
+     */
+    var calcLastre = function(num, price) {
+        return currencyAdapter.getCurrencyValue(
+            num * price);
+    };
+
+    /**
+     * @param i integer interasse [m]
+     * @param d integer viti distance [m]
+     * @param price float the single vite price [euro@vite]
+     */
+    var calcViti = function(i, d, price) {
+        if (i == '0' || d == '0') {
+            return 0;
+        }
+        return currencyAdapter.getCurrencyValue(
+            price / (i * d));
+    };
+
+    /**
+     * @param price float the stucco price [euro@m^2]
+     */
+    var calcStucco = function(price) {
+        return currencyAdapter.getCurrencyValue(
+            2 * price);
+    };
+
+    /**
+     * @param price float the lana di roccia price [euro@m^2]
+     */
+    var calcRoccia = function(price) {
+        return currencyAdapter.getCurrencyValue(price);
+    };
+
+    /**
+     * @param b integer width [m]
+     * @param h integer height [m]
+     * @param d integer distance between tasselli [m]
+     * @param price float the guide price [euro@tassello]
+     */
+    var calcTasselli = function(b, h, d, price) {
+        if (b == '0' || h == '0' || d == '0') {
+            return 0;
+        }
+        return currencyAdapter.getCurrencyValue(
+            2 * (b + h) * price / (b * h * d));
+    };
+
+    /**
+     * @param b float width [m]
+     * @param h float height [m]
+     * @param i float interasse [m]
+     * @param dViti float distance between viti [m]
+     * @param dTasselli float distance between tasselli [m]
+     * @param numLastre integer number of lastre [dimensionless]
+     * ... various prices
+     */
+    var calcTotale = function(b, h, i, dViti, dTasselli, numLastre,
+        pMontante, pGuida, pLastra, pViti, pStucco, pRoccia, pTasselli){
+
+        return calcMontanti(i, pMontante) +
+            calcGuide(b, h, pGuida) +
+            calcLastre(numLastre, pLastra) +
+            calcViti(i, dViti, pViti) +
+            calcStucco(pStucco) +
+            calcRoccia(pRoccia) +
+            calcTasselli(b, h, dTasselli, pTasselli)
+            ;
     };
 
     return {
         calcMontanti: calcMontanti,
-        calcGuide: calcGuide
+        calcGuide: calcGuide,
+        calcLastre: calcLastre,
+        calcViti: calcViti,
+        calcStucco: calcStucco,
+        calcRoccia: calcRoccia,
+        calcTasselli: calcTasselli,
+        calcTotale: calcTotale
     };
 }]);
 
--- test/unit/serviceSpec.js
+++ test/unit/serviceSpec.js
@@ -13,22 +13,153 @@ describe('currencyAdapter service', function() {
     });
   });
 
+
   it('check the existence of currencyAdapter service', function() {
     expect(currencyAdapter).toBeDefined();
   });
 
+
   it('check the currencyAdapter interface', function() {
     expect(currencyAdapter.getCurrencyValue).toBeDefined();
   });
+
 
   it('test the currencyAdapter logic', function() {
     expect(currencyAdapter.getCurrencyValue(1.23)).toBe(1.23);
     expect(currencyAdapter.getCurrencyValue(1.234)).toBe(1.23);
     expect(currencyAdapter.getCurrencyValue(1.235)).toBe(1.24);
     expect(currencyAdapter.getCurrencyValue('1.23')).toBe(1.23);
-    expect(currencyAdapter.getCurrencyValue('cartongesso')).toBeNaN();
+    expect(currencyAdapter.getCurrencyValue('cartongesso')).toBe(0);
   });
 
 });
+
+
+describe('cartongessoCalculator service', function() {
+  var cartongessoCalculator;
+
+  beforeEach(function(){
+    module('cartongessoServices');
+
+    inject(function (_cartongessoCalculator_) {
+      cartongessoCalculator = _cartongessoCalculator_;
+    });
+  });
+
+
+  it('check the existence of cartongessoCalculator service', function() {
+    expect(cartongessoCalculator).toBeDefined();
+  });
+
+
+  it('test the cartongessoCalculator calcMontanti', function() {
+    expect(cartongessoCalculator.calcMontanti).toBeDefined();
+
+    var price = 3,
+        i = 7;
+    // the result should be:
+    // price / i
+    expect(cartongessoCalculator.calcMontanti(i, price)).toBe(0.43);
+
+    price = 1; i = 0;
+    expect(cartongessoCalculator.calcMontanti(i, price)).toBe(0.00);
+  });
+
+
+  it('test the cartongessoCalculator calcGuide', function() {
+    expect(cartongessoCalculator.calcGuide).toBeDefined();
+
+    var b = 3,
+        h = 1,
+        price = 1;
+    // the result should be:
+    // (2 * (b + h) * price) / (b * h)
+    expect(cartongessoCalculator.calcGuide(b, h, price)).toBe(2.67);
+
+    b = 0;
+    expect(cartongessoCalculator.calcGuide(b, h, price)).toBe(0.00);
+
+    b = 3; h = 0;
+    expect(cartongessoCalculator.calcGuide(b, h, price)).toBe(0.00);
+  });
+
+
+  it('test the cartongessoCalculator calcLastre', function() {
+    expect(cartongessoCalculator.calcLastre).toBeDefined();
+
+    var num = 2,
+        price = 1;
+    // the result should be:
+    // num * price
+    expect(cartongessoCalculator.calcLastre(num, price)).toBe(2.00);
+  });
+
+
+  it('test the cartongessoCalculator calcViti', function() {
+    expect(cartongessoCalculator.calcViti).toBeDefined();
+
+    var i = 0.3,
+        d = 0.4,
+        price = 0.1;
+    // the result should be:
+    // price / (i * d)
+    expect(cartongessoCalculator.calcViti(i, d, price)).toBe(0.83);
+
+    i = 0;
+    expect(cartongessoCalculator.calcViti(i, d, price)).toBe(0.00);
+
+    i = 1; d = 0;
+    expect(cartongessoCalculator.calcViti(i, d, price)).toBe(0.00);
+  });
+
+
+  it('test the cartongessoCalculator calcStucco', function() {
+    expect(cartongessoCalculator.calcStucco).toBeDefined();
+
+    var price = 3.45;
+    // the result should be:
+    // 2 * price
+    expect(cartongessoCalculator.calcStucco(price)).toBe(6.90);
+  });
+
+
+  it('test the cartongessoCalculator calcRoccia', function() {
+    expect(cartongessoCalculator.calcRoccia).toBeDefined();
+
+    var price = 3.45;
+    // the result should be:
+    // price
+    expect(cartongessoCalculator.calcRoccia(price)).toBe(3.45);
+  });
+
+
+  it('test the cartongessoCalculator calcTasselli', function() {
+    expect(cartongessoCalculator.calcTasselli).toBeDefined();
+
+    var b = 1,
+        h = 1,
+        price = 1,
+        d = 1;
+    // the result should be:
+    // (2 * (b + h) * price) / (d * b * h)
+    expect(cartongessoCalculator.calcTasselli(b, h, d, price)).toBe(4.00);
+
+    b = 0;
+    expect(cartongessoCalculator.calcTasselli(b, h, d, price)).toBe(0.00);
+
+    b = 1; h = 0;
+    expect(cartongessoCalculator.calcTasselli(b, h, d, price)).toBe(0.00);
+
+    h = 1; d = 0;
+    expect(cartongessoCalculator.calcTasselli(b, h, d, price)).toBe(0.00);
+  });
+
+
+  it('test the cartongessoCalculator calcTotale', function() {
+    expect(cartongessoCalculator.calcTotale).toBeDefined();
+  });
+
+});
+
 
 })();
